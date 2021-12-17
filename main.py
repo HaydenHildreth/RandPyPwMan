8182486b3da80885cb0685577c7884e77f1cd9e4
HaydenHildreth <36833433+HaydenHildreth@users.noreply.github.com>
2021-12-17 18:54:11 -0500

    Added default value to pw_length value
    Added if and else statement

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ from tkinter import *
 
 
 window = Tk()
-window.title('RandPyPwGen v.0.2')
+window.title('RandPyPwGen v.0.1')
 window.geometry("800x600")
 name = os.getlogin()
 alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 password = ""
-pw_len = 0
+pw_len = 12  # Default length if not specified
 entry_len = StringVar()
 
 
 def click():
     global password
     global pw_len
-    pw_len = int(entry_len.get())
+    if not entry_len.get():
+        pass
+    else:
+        pw_len = int(entry_len.get())
     password = ''.join(secrets.choice(alphabet) for i in range(pw_len))
     print_pw.configure(text=f"Your password is {password}")
 
@@ -34,8 +37,6 @@ input_text = Entry(window, textvariable=entry_len)
 input_text.pack()
 print_pw = Label(window, text=f"Your password is: {password}")
 print_pw.pack()
-
-
 sendBtn = Button(window, text="Generate!", command=click)
 copyBtn = Button(window, text="Copy!", command=copy)
 sendBtn.pack(side=BOTTOM)
