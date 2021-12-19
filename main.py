90483a412fce8745993361e896e8b9d32ff07d7c
HaydenHildreth <36833433+HaydenHildreth@users.noreply.github.com>
2021-12-18 20:02:18 -0500

    Removed default value of pw_length
    This conflicted with the raise exception -> ValueError, which happens when nothing is entered or a string is entered.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import string
 import secrets
 import tkinter.messagebox
-
 import pyperclip
 from tkinter import *
 
@@ -13,21 +12,13 @@ window.geometry("800x600")
 name = os.getlogin()
 alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 password = ""
-pw_len = 12  # Default length if not specified
+pw_len = 0
 entry_len = StringVar()
 
 
 def click():
     global password
     global pw_len
-    '''
-    if not entry_len.get():
-        pass
-    else:
-        pw_len = int(entry_len.get())
-    password = ''.join(secrets.choice(alphabet) for i in range(pw_len))
-    print_pw.configure(text=f"Your password is {password}")
-    '''
     try:
         pw_len = int(entry_len.get())
         if pw_len > 100 or pw_len <= 0:
@@ -37,9 +28,8 @@ def click():
         print_pw.configure(text=f"Your password is {password}")
     except ValueError:
         tkinter.messagebox.showerror(title="Invalid input", message="Enter an integer...")
-        print('Enter an integer...')
     except RuntimeError:
-        tkinter.messagebox.showerror(title="Invalid input", message="Password length must be non-negative and less than 100 characters")
+        tkinter.messagebox.showerror(title="Invalid input", message="Number must be positive and under 101 characters")
 
 
 def copy():
