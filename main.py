5187dad45a9c8d73f0fcbb94935cb093f5995dd3
HaydenHildreth <36833433+HaydenHildreth@users.noreply.github.com>
2022-10-24 09:30:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import string
 import secrets
 import tkinter.messagebox
 import pyperclip
+import passlib
 import tkinter as tk
+from tkinter.ttk import *
 
 window = tk.Tk()
 window.title('RandPyPwGen v.0.1')
@@ -13,6 +15,7 @@ alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + str
 password = ""
 pw_len = 0
 entry_len = tk.StringVar()
+columns = ('Site name', 'Username', 'Password')
 
 
 def click():
@@ -39,13 +42,18 @@ def copy():
 greeting = tk.Label(window, text=f"Greetings {name}.")
 greeting.grid(row=0, column=0, sticky=tk.W)
 t = tk.Label(window, text="Please input desired password length:")
-t.grid(row=1, column=1, sticky=tk.E + tk.W)
+t.grid(row=1, column=0, sticky=tk.E + tk.W)
 input_text = tk.Entry(window, textvariable=entry_len)
-input_text.grid(row=1, column=2, sticky=tk.E + tk.W)
+input_text.grid(row=1, column=1, sticky=tk.E + tk.W)
 print_pw = tk.Label(window, text=f"Your password is: {password}")
-print_pw.grid(row=1, column=1, sticky=tk.E + tk.W)
+print_pw.grid(row=2, column=0, sticky=tk.E + tk.W)
 sendBtn = tk.Button(window, text="Generate!", command=click)
 copyBtn = tk.Button(window, text="Copy!", command=copy)
-sendBtn.grid(row=2, column=1, sticky=tk.E + tk.W)
-copyBtn.grid(row=2, column=2, sticky=tk.E + tk.W)
+sendBtn.grid(row=3, column=0, sticky=tk.E + tk.W)
+copyBtn.grid(row=3, column=1, sticky=tk.E + tk.W)
+tvData = Treeview(window, columns=columns, show='headings')
+tvData.grid(row=4, column=0, columnspan=5)
+tvData.heading('Site name', text='Site name')
+tvData.heading('Username', text='Username')
+tvData.heading('Password', text='Password')
 window.mainloop()
