3dba4321575c36de285944b4b2f23c18cf529a62
Hayden Hildreth <36833433+HaydenHildreth@users.noreply.github.com>
2023-06-17 13:21:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ from cryptography.fernet import Fernet
 from tkinter.ttk import *
 
 
-"""
-Draw main window
-Create alphabet var of possible char types
-Initialize pw variable, len, and column var(s)
-"""
 window = tk.Tk()
 window.title('RandPyPwGen v.0.6.1')
 window.geometry('800x600')
@@ -24,26 +19,23 @@ columns = ('ID', 'Site name', 'Username', 'Password')
 site_name = ''
 username = ''
 password_str = ''
+group = ''
 
-
-"""
-See if db engine is installed and db folder exists
-"""
 try:
-    conn = sqlite3.connect('db/data.db')
+    conn = sqlite3.connect('./db/data.db')
     c = conn.cursor()
     c.execute("SELECT * FROM data")
     records = c.fetchall()
 except sqlite3.OperationalError:
+    # THIS ERROR COULD BE MADE BETTER, IS SQL LITE INSTALLED, OR IS INSTALL.PY NOT RAN? MAKE MORE INFORMATIONAL
+    # I.E. DATABASE FOLDER NOT FOUND, PLEASE RUN INSTALL.PY BEFORE EXECUTING PROGRAM
+    # OR SQLITE3 NOT FOUND, PLEASE SEE DOCUMENTATION AND INSTALL BEFORE USING PROGRAM
     tkinter.messagebox.showerror(title="SQLite not installed", message="Please install SQLite before use.")
     exit()
 
 
-"""
-See if encryption key exists
-"""
 try:
-    conn2 = sqlite3.connect('db/unlock.db')
+    conn2 = sqlite3.connect('./db/unlock.db')
     c2 = conn2.cursor()
     c2.execute("SELECT enc_key FROM master")
     records2 = c2.fetchall()
@@ -63,7 +55,6 @@ except sqlite3.OperationalError:
 # Make UI fit screen (grow/shrink with window size)
 
 
-# Called when the "Generate" button is clicked
 def click():
     global password
     global pw_len
@@ -80,14 +71,15 @@ def click():
         tkinter.messagebox.showerror(title="Invalid input", message="Number must be positive and under 101 characters")
 
 
-# Draws a new window, called when "Add" button is clicked
 def new_window():
     global site_name
     global username
     global password_str
+    global group
     global ipsn
     global ipun
     global ippw
+    global ipgroup
     global new
     new = tk.Toplevel(window)
     new.title("Add new site...")
@@ -104,6 +96,10 @@ def new_window():
     lblpw.grid()
     ippw = tk.Entry(new, textvariable=password_str)
     ippw.grid()
+    lblgroup = Label(new, text="Group:")
+    lblgroup.grid()
+    ipgroup = tk.Entry(new, textvariable=group)
+    ipgroup.grid()
     btnSubmit = Button(new, text="Add account", command=insert_info)
     btnSubmit.grid()
     btnCancel = Button(new, text="Cancel", command=cancel_button)
@@ -113,36 +109,33 @@ def new_window():
     ippw.insert(0, password)
 
 
-# This function inserts data into the treeview, and the data table
 def insert_info():
     global index
     site_name = ipsn.get()
     username = ipun.get()
     password_str = ippw.get()
+    group = ipgroup.get()
 
     pw_copy = bytes(password_str, 'utf-8')
     enc = f.encrypt(pw_copy)
 
-    tvData.insert(parent='', index='end', values=(index, site_name, username, password_str))
-    c.execute("INSERT INTO data VALUES (?,?,?,?)", (index, site_name, username, enc))
+    tvData.insert(parent='', index='end', values=(index, site_name, username, password_str, group))
+    c.execute("INSERT INTO data VALUES (?,?,?,?,?)", (index, site_name, username, enc, group))
     index = index + 1
     conn.commit()
     new.destroy()
 
 
-# This function clears out data in the input boxes of the new window screen
 def cancel_button():
     ipsn.delete(0, 'end')
     ipun.delete(0, 'end')
     ippw.delete(0, 'end')
 
 
-# This function calls the new window, called when "Add" button is clicked
 def add_button():
     new_window()
 
 
-# Copy selected treeview pw to clipboard, called when "Copy" button is clicked
 def copy():
     sel = tvData.selection()[0]
     item = tvData.item(sel)
@@ -151,7 +144,6 @@ def copy():
     pyperclip.copy(val)
 
 
-# Removes records from table, called when "Delete" button is clicked
 def deleteRecord():
     try:
         sel = tvData.selection()[0]
@@ -165,19 +157,22 @@ def deleteRecord():
         tkinter.messagebox.showerror(title="Cannot delete record", message="Please choose a record to delete.")
 
 
-# Opens a edit screen
 def editRecord():
     global ipsn
     global ipun
     global ippw
+    global ipgroup
     global new
     try:
         cur = tvData.focus()
         v = tvData.item(cur)
+        print(cur)
+        print(v)
         d = v['values']
         sn = d[1]
         un = d[2]
         pw = d[3]
+        # group = d[4]
         new = tk.Toplevel(window)
         new.title("Add new site...")
         new.geometry("200x200")
@@ -193,6 +188,10 @@ def editRecord():
         lblpw.grid()
         ippw = tk.Entry(new, textvariable=password_str)
         ippw.grid()
+        lblgroup = Label(new, text="Group:")
+        lblgroup.grid()
+        ipgroup = tk.Entry(new, textvariable=group)
+        ipgroup.grid()
         btnSubmit = Button(new, text="Update", command=update_info)
         btnSubmit.grid()
         btnCancel = Button(new, text="Cancel", command=cancel_edit)
@@ -202,59 +201,66 @@ def editRecord():
         ipsn.insert(0, sn)
         ipun.insert(0, un)
         ippw.insert(0, pw)
+        ipgroup.insert(0, group)
     except IndexError:
         tkinter.messagebox.showerror(title="Cannot edit record", message="Please choose a record to edit.")
 
 
-# Closes out new window
 def exit_button():
     new.destroy()
 
 
-# This function sends updated information from the "Edit" button/function
 def update_info():
     global ipsn
     global ipun
     global ippw
+    global ipgroup
     global new
     sel = tvData.focus()
     item = tvData.item(sel)
     get_values = item['values']
     selected_index = get_values[0]
-    val = tvData.item(sel, values=(selected_index, ipsn.get(), ipun.get(), ippw.get()))
+    val = tvData.item(sel, values=(selected_index, ipsn.get(), ipun.get(), ippw.get(), ipgroup.get()))
 
     pw_copy = bytes(ippw.get(), 'utf-8')
     enc = f.encrypt(pw_copy)
 
     values = []
-    values.append(ipsn.get())
-    values.append(ipun.get())
+    temp_sn = ipsn.get()
+    temp_un = ipun.get()
+    temp_pw = ippw.get()
+    temp_group = ipgroup.get()
+    values.append(temp_sn)
+    values.append(temp_un)
     values.append(enc)
-    c.execute("UPDATE data SET site = (?), username = (?), password = (?) WHERE id = (?)", (values[0], values[1], values[2], selected_index))
+    values.append(temp_group)
+    c.execute("UPDATE data SET site = (?), username = (?), password = (?), group_val = (?) WHERE id = (?)", (values[0], values[1], values[2], values[3], selected_index))
     conn.commit()
     new.destroy()
 
 
-# Clears out the field in input boxes in edit screen
 def cancel_edit():
     global ipsn
     global ipun
     global ippw
+    global ipgroup
     cur = tvData.focus()
     v = tvData.item(cur)
     d = v['values']
     sn = d[1]
     un = d[2]
     pw = d[3]
+    gr = d[4]
     ipsn.delete(0, 'end')
     ipun.delete(0, 'end')
     ippw.delete(0, 'end')
+    ipgroup.delete(0, 'end')
     ipsn.insert(0, sn)
     ipun.insert(0, un)
     ippw.insert(0, pw)
+    ipgroup.insert(0, gr)
 
 
-# Draw graphics to main window
 t = tk.Label(window, text="Please input desired password length:")
 t.grid(row=1, column=0, sticky=tk.E + tk.W)
 input_text = tk.Entry(window, textvariable=entry_len)
@@ -294,7 +300,6 @@ editBtn.config(height=3)
 copyBtn.config(height=3)
 
 
-# Decrypt pw for treeview
 count = 0
 for i in records:
     decrypted = f.decrypt(records[count][3])
@@ -303,11 +308,10 @@ for i in records:
     count += 1
 
 
-# Get all pw from data table
 last = c.execute("SELECT * FROM data ORDER BY id DESC LIMIT 1")
 last = c.fetchone()
 
-# Set index to last pw or 0
+
 if last is None:
     index = 0
 else:
