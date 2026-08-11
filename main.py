c5dfa490c09f970de2b983a9f0b862d880ef7314
Hayden Hildreth <36833433+HaydenHildreth@users.noreply.github.com>
2026-08-11 11:25:24 -0400

    Fix version number
    Oops, last build compiled fine anyhow.

--- main.py
+++ main.py
@@ -2083,7 +2083,7 @@ class MainFrame(ttk.Frame, ThemedWidget):
     def _show_about(self):
         """Show about window"""
         self._register_activity()
-        messagebox.showinfo("About", "RandPyPwGen v2.0.0\nA secure password manager\n\nBy Hayden Hildreth")
+        messagebox.showinfo("About", "RandPyPwGen v2.0.1\nA secure password manager\n\nBy Hayden Hildreth")
     
     def _open_help(self):
         """Open help in browser"""
@@ -3468,7 +3468,7 @@ class PasswordManagerApp:
     
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("RandPyPwGen v2.0.0")
+        self.root.title("RandPyPwGen v2.0.1")
         self.root.geometry("900x700")
         
         self.root.update_idletasks()
@@ -3501,7 +3501,7 @@ class PasswordManagerApp:
             self.current_frame.destroy()
         
         self.root.geometry("900x700")
-        self.root.title("RandPyPwGen v2.0.0")
+        self.root.title("RandPyPwGen v2.0.1")
         
         self.current_frame = MainFrame(self.root, self.db_manager, self._show_login)
         self.current_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
